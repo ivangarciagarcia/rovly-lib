5ff0c4aaf567ad6b613178807a4709a375ae0a61
ivangarciagarcia <garciaivan200@gmail.com>
2025-01-07 18:33:22 +0100

    test

--- rovly-lib-clerk/src/test/java/dev/ivangg/rovly/lib/clerk/ClerkApiClientTester.java
+++ rovly-lib-clerk/src/test/java/dev/ivangg/rovly/lib/clerk/ClerkApiClientTester.java
@@ -3,6 +3,7 @@ package dev.ivangg.rovly.lib.clerk;
 import dev.ivangg.rovly.lib.clerk.model.ClerkUser;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ClerkApiClientTester {
@@ -11,10 +12,13 @@ public class ClerkApiClientTester {
 
         ClerkApiClient client = new ClerkApiClient("Secret Key");
 
-        List<ClerkUser> clerkUsers = client.listUsers(null);
+        List<String> emailList = new ArrayList<>();
+        emailList.add("user email");
+
+        List<ClerkUser> clerkUsers = client.listUsers(emailList);
         System.out.println("USER LIST: " + clerkUsers);
 
-        ClerkUser clerkUser = client.getUser("userId");
+        ClerkUser clerkUser = client.getUser(clerkUsers.getFirst().getId());
         System.out.println("ID: " + clerkUser.getId());
         System.out.println("NAME: " + clerkUser.getName());
 
